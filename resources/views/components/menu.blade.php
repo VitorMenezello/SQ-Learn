89f7e9b1d5d8806b2dbc1720a13ab52d63d62726
Vitor Menezello <vitormenezello@gmail.com>
2018-06-03 19:29:42 -0300

    Adicionados diversos componentes
    Mudança do menu e criação de componentes

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,18 @@
+<div layout="row" layout-align="center end" class="header-container">
+
+    <div flex="20" class="logo">
+        <!-- img -->
+        <h1>
+            SQ-Learn
+        </h1>
+    </div>
+
+    <div flex="80" class="menu" layout="column">
+        <ul>
+            <li flex>Tutorial</li>
+            <li flex>Praticando</li>
+            <li flex>SQ-Look</li>
+        </ul>
+    </div>
+
+</div>
