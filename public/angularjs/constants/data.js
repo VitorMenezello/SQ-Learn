a74c26bb7ea87b5cf9c6c6368e6693120c436d49
Marinammm <marinamoreirammm30@gmail.com>
2018-06-12 17:27:04 -0300

    database

---
+++ public/angularjs/constants/data.js
@@ -0,0 +1,162 @@
+
+alunos = 
+[
+{
+  "nome": "Monique Valadares"
+  "matrícula": "2013007453"
+  "sexo": "F"
+}
+{
+  "nome": "Clécio Araújo"
+  "matrícula":"2014030212"
+  "sexo": "M"
+}
+{
+  "nome": "Raíssa Costa"
+  "matrícula":"2018005678"
+  "sexo": "F"
+}
+{
+  "nome": "Irina Ceará"
+  "matrícula": "2015003765"
+  "sexo": "F"
+}
+{
+  "nome": "Pablo Ribeiro"
+  "matrícula": "2013007689"
+  "sexo": "M"
+}
+{
+  "nome": "Ravi Martins"
+  "matrícula": "2013007345"
+  "sexo": "M"
+}
+{
+  "nome": "Lubyanka Moreira"
+  "matrícula": "2015004349"
+  "sexo": "F"
+}
+{
+  "nome": "Dayse Aparotto"
+  "matrícula": "2016008934"
+  "sexo": "F"
+}
+{
+  "nome": "Angélica Santos"
+  "matrícula": "2013007269"
+  "sexo": "F"
+}
+{
+  "nome": "Francisco Dias"
+  "matrícula": "2009007845"
+  "sexo": "M"
+}
+]
+
+materias =
+[
+{
+  "nome": "Poções 1"
+  "código": "CDD023"
+}
+{
+  "nome": "Poções 2"
+  "código": "CDD011"
+}
+{
+  "nome": "Toelogia Abstrata"
+  "código": "CDD765"
+}
+{
+  "nome": "Introdução à Vida Aquática"
+  "código": "CDD493"
+}
+{
+  "nome": "Ciência de Drones Avançado"
+  "código": "CDD926"
+}
+]
+
+matriculado_em =
+[
+{
+  "idAluno":"2013007453"
+  "idMateria":"CDD765"
+}
+{
+  "idAluno":"2018005678"
+  "idMateria":"CDD765"
+}
+{
+  "idAluno":"2013007345"
+  "idMateria":"CDD926"
+}
+{
+  "idAluno":"2013007689"
+  "idMateria":"CDD011"
+}
+{
+  "idAluno":"Irina Ceará"
+  "idMateria":"CDD011"
+}
+{
+  "idAluno":"2013007453"
+  "idMateria":"CDD926"
+}
+{
+  "idAluno":"2013007345"
+  "idMateria":"CDD493"
+}
+{
+  "idAluno":"2013007689"
+  "idMateria":"CDD926"
+}
+{
+  "idAluno":"2015004349"
+  "idMateria":"CDD926"
+}
+{
+  "idAluno":"2014030212"
+  "idMateria":"CDD765"
+}
+{
+  "idAluno":"Irina Ceará"
+  "idMateria":"CDD765"
+}
+{
+  "idAluno":"2016008934"
+  "idMateria":"CDD011"
+}
+{
+  "idAluno":"2014030212"
+  "idMateria":"CDD493"
+}
+{
+  "idAluno":"2015004349"
+  "idMateria":"CDD493"
+}
+{
+  "idAluno":"2013007269"
+  "idMateria":"CDD011"
+}
+{
+  "idAluno":"2016008934"
+  "idMateria":"CDD926"
+}
+{
+  "idAluno":"2013007269"
+  "idMateria":"CDD926"
+}
+{
+  "idAluno":"2009007845"
+  "idMateria":"CDD023"
+}
+{
+  "idAluno":"2018005678"
+  "idMateria":"CDD023"
+}
+{
+  "idAluno":"2009007845"
+  "idMateria":"CDD493"
+}
+]
