e9424aadcc8f68fc786c23142a18fc21691c3331
Vitor Menezello <vitormenezello@gmail.com>
2018-11-24 23:20:30 -0200

    Homepage

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,27 @@
-<div id="footer" class="footer-container" layout="row" layout-align="center center">
-    <p>Desenvolvido por Vitor Menezello sob orientação da professora Mirella M. Moro</p>
+<div id="footer" class="footer-container" layout="row">
+
+    <div flex="20" class="logo" layout="column" layout-align="center center">
+
+        <img src="{{ url('/images/sqlearn-logo-branco.png') }}">
+
+    </div>
+
+    <div flex="40" class="text" layout="column" layout-align="center center">
+        Encontrou algum problema ou possui alguma sugestão?
+    </div>
+
+    <div flex="20" class="text" layout="row" layout-align="center center">
+        <a href="mailto:vitormenezello@gmail.com">
+            <md-icon md-svg-src="{{ url('/icons/email.svg') }}"></md-icon>
+            vitormenezello@gmail.com
+        </a>
+    </div>
+
+    <div flex="20" class="text" layout="row" layout-align="center center">
+        <a href="https://github.com/VitorMenezello/SQ-Learn">
+            <md-icon md-svg-src="{{ url('/icons/github-box.svg') }}"></md-icon>
+            @vitormenezello
+        </a>
+    </div>
+
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,17 +7,57 @@
 @endsection
 
 @section('stylesheets')
+    <link rel="stylesheet" href="{{ url('/css/index.css') }}">
 @endsection
 
 @section('body')
 
     <div ng-controller="IndexController" id="index">
 
-        <md-content class="content-container" layout="row">
+        <md-content class="index-container">
 
-            Todo
+            <div class="section s-1" layout="row">
+                <div class="section-text" flex="60">
+                    <h3>Novo SQLearn</h3>
+                    <p>
+                        O SQLearn foi desenvolvido para substituir o antigo site Praticando SQL. A plataforma possui os
+                        mesmos objetivos, mas realizamos uma mudança completa no visual e adicionamos diversas novas
+                        funcionalidades ao site, incluindo um tutorial próprio.
+                    </p>
+                </div>
+                <img flex="40" src="{{ url('/images/bancodedados.svg') }}">
+            </div>
+
+            <div class="section s-2" layout="row">
+                <img flex="40" src="{{ url('/images/caixas.svg') }}">
+                <div class="section-text" flex="60">
+                    <h3>SQLook</h3>
+                    <p>
+                        O novo site conta um Construtor Visual de Consultas SQL, chamado de SQLook. Para os iniciantes,
+                        ela é uma plataforma perfeita para começar a criar suas consultas e entender o funcionamento da
+                        linguagem sem a necessidade de conhecer a sintaxe previamente.
+                    </p>
+                </div>
+            </div>
+
+            <div class="section s-3" layout="row">
+                <div class="section-text" flex="60">
+                    <h3>Atualizações</h3>
+                    <p>
+                        Além disso, o site receberá constantes atualizações com melhorias e mais ferramentas para
+                        tornar os seus estudos de SQL mais fáceis ainda. Todo o código do site ficará em repositório
+                        aberto ao público para que todos possam reportar bugs e erros.
+                    </p>
+                </div>
+                <img src="{{ url('/images/engrenagemmagra.svg') }}">
+            </div>
 
         </md-content>
+
+        <div>
+            @component('components.footer')
+            @endcomponent
+        </div>
 
     </div>
 
